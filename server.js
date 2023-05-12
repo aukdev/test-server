bbc31e75c222d59854274898d3b85c96b5491500
aukdev <amilaupul344@outlook.com>
2023-05-12 16:53:19 +0530

    get method

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
+app.get("/", (req, res) => res.status(200).json({ msg: "hi" }));
+
 app.post("/test", (req, res) => res.status(200).json({ msg: "hi" }));
 
 app.post("/create-payment-intent", async (req, res) => {
